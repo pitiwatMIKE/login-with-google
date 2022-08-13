09f0c1ed5f7ea0cb57e46f57cf00fd7786899fd3
Pitiwat Promnimit <pitiwat.pr61@gmail.com>
2022-08-13 23:13:30 +0700

    login with google success

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function Home() {
+  let [searchParams] = useSearchParams();
+  let navigate = useNavigate();
+  useEffect(() => {
+    let token = searchParams.get("token");
+    if (token) {
+      localStorage.setItem("auth", token);
+      navigate("/profile");
+    }
+  }, [searchParams]);
+
+  const googleAuth = () => {
+    window.open(
+      process.env.REACT_APP_API_URL + "/auth/google/callback",
+      "_self"
+    );
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button type="button" onClick={googleAuth}>
+        Login with google
+      </button>
+    </div>
+  );
+}
